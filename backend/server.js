b9d44c5f4bb08b2258d4b6cee6449d0485426058
Gagan Kapoor <gagankapoor092485@gmail.com>
2026-03-20 19:54:06 +0530

    server changes

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,9 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model("User", userSchema);
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER, // your gmail
     pass: process.env.EMAIL_PASS,   // app password
